8f1adef7db402df2f605d514f7d5f427420602e0
VM18042 <vm18042@ues.edu.sv>
2021-04-08 09:38:29 -0600

    Ajuste del Container

--- Container/src/container/Container.java
+++ Container/src/container/Container.java
@@ -1,21 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package container;
-
-/**
- *
- * @author larok
- */
-public class Container {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+import java.util.LinkedList;
+import java.util.List;
     
+public class Container {
+    
+    List<Package> contents;
+    
+
+    public Container() {
+        contents = new LinkedList<Package>();
+    }
+
+    public boolean add(Package b) {
+        return !contents.contains(b) && contents.add(b);
+    }
+
+    public boolean remove(Package b) {
+        return contents.remove(b);
+    }
+
+    public double getVolume() {
+        int sum = 0;
+        for (Package b : contents) {
+            sum += b.getVolume();
+        }
+        return sum;
+    }
+
+    public int size() {
+        return contents.size();
+    }
+
+    public void clear() {
+        contents.clear();
+    }
+
+    public boolean contains(Package b) {
+        return contents.contains(b);
+    }
 }
